94cd4ab05f0690953073095d3418efb9eb8c9c35
mmcintyre1 <mfmcintyre01@gmail.com>
2022-04-08 17:17:06 -0400

    added some ch 1 exercises

--- chapter1/9.c
+++ chapter1/9.c
@@ -1,1 +1,13 @@
-#define <stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+
+main() {
+  int c, prevchar;
+  prevchar = 0;
+  while ((c = getchar()) != EOF) {
+    if (!(isblank(c) && isblank(prevchar))) {
+      putchar(c);
+      prevchar = c;
+    }
+  }
+}
